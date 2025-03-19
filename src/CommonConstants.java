5cc002990564d001bf4ebc012547f42d2c8462e1
Psajdak <psajdak1000@gmail.com>
2025-03-19 17:22:37 +0100

    dalej dalej

--- src/CommonConstants.java
+++ src/CommonConstants.java
@@ -2,5 +2,23 @@ import java.awt.*;
 
 public class CommonConstants {
     //frame config
-    public static final Dimension FRAME_SIZE = new Dimension(440,660);
+    public static final Dimension GUI_SIZE =
+            new Dimension(440,660);
+
+    //banner config
+    public static final Dimension BANNER_SIZE =
+            new Dimension(GUI_SIZE.width,50);
+
+    //task panel config
+
+    public static final Dimension TASKPANEL_SIZE = new Dimension(
+            GUI_SIZE.width - 30,
+            GUI_SIZE.height - 175);
+
+    //add task button config
+    public static final Dimension ADDTASK_BUTTON_SIZE = new Dimension(
+            GUI_SIZE.width,50);
+
+
+
 }
---
+++ src/TaskComponent.java
@@ -0,0 +1,5 @@
+import javax.swing.*;
+
+public class TaskComponent extends JPanel {
+
+}
